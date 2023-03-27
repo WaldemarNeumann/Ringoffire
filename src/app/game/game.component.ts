67a4cbefa0bba4197d2200ad4b69325de4f9e07e
WaldemarNeumann <113894308+WaldemarNeumann@users.noreply.github.com>
2023-03-27 09:57:54 +0200

    create the steack

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '@angular/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+}
