7fc10e71d0bff8a07d79f68d7340e6f0004145c4
WaldemarNeumann <113894308+WaldemarNeumann@users.noreply.github.com>
2023-03-30 12:05:13 +0200

    create firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-e8ce0',
+    appId: '1:7230405978:web:2f2750af7c6e1937948763',
+    storageBucket: 'ring-of-fire-e8ce0.appspot.com',
+    apiKey: 'AIzaSyAbWTQlEjQgV9CzOA9lD-hnFR82UVTfMFQ',
+    authDomain: 'ring-of-fire-e8ce0.firebaseapp.com',
+    messagingSenderId: '7230405978',
+  },};
